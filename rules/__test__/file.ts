c004623d6e75e93cc6e2b8ee34e7afca44677b5d
bailixing <bailixing@pinduoduo.com>
2023-04-22 18:07:04 +0800

    feat(): remove unused

--- rules/__test__/file.ts
+++ rules/__test__/file.ts
@@ -1,21 +1,36 @@
 import { ESLintUtils } from '@typescript-eslint/utils';
-import rule from '../no-raw-number-calculation';
+import importsRule from '../../lib/rules/fix-no-unused-imports';
+import varsRule from '../../lib/rules/fix-no-unused-vars';
 
 const ruleTester = new ESLintUtils.RuleTester({
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.json',
-  },
+  // parserOptions: {
+  //   tsconfigRootDir: __dirname,
+  //   project: './tsconfig.json',
+  // },
 });
-ruleTester.run('my-typed-rule', rule, {
+ruleTester.run('no-unused-imports', importsRule, {
   valid: [
-    "const a = 1; const b = '2'; console.log(a + b);"
+    "import { a } from 'react'; const x = a;"
   ],
   invalid: [
     {
-      code: "const a = 2; const b = 4; console.log(a + b)",
-      errors: [{ messageId: 'noRawNumberCalculation' }]
+      code: "import { a } from 'react'; console.log('unused');",
+      output: "console.log('unused');",
+      errors: [{ messageId: 'unusedVar' }]
     }
   ],
 });
+
+// ruleTester.run('no-unused-vars', varsRule, {
+//   valid: [
+//     "const a = 3; console.log(a);"
+//   ],
+//   invalid: [
+//     {
+//       code: "const a = 3,b=2; console.log(b);",
+//       output: "const b=2; console.log(b);",
+//       errors: [{ messageId: 'unusedVar' }]
+//     }
+//   ],
+// });
